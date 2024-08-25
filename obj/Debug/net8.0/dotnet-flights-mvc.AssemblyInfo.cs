3fc433b29ce3e62b319cb74ba0d3507b8ae857aa
jurgenstevens <jurgen.stevens@yahoo.com>
2024-08-24 22:01:42 -0500

    Update Home Index view text welcoming user to Flights Dot Net.

--- obj/Debug/net8.0/dotnet-flights-mvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-flights-mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-flights-mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a078a67cc21ea5185f56e64f933d07d5fa572744")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1195ab1a881b33cac32efbc5497b644bee95361e")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-flights-mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-flights-mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
