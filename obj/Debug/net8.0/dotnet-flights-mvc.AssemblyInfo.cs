a01d85506b6397097747e5e75474ba8eeb4a6c44
jurgenstevens <jurgen.stevens@yahoo.com>
2024-08-26 23:56:29 -0500

    Update the color of the nav bar and footer to black.

--- obj/Debug/net8.0/dotnet-flights-mvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-flights-mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-flights-mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fc433b29ce3e62b319cb74ba0d3507b8ae857aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2276ad4fb36927393cce5418f63c82851ad8076a")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-flights-mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-flights-mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
