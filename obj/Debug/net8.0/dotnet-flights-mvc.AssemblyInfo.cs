21d761ce40d0d22a7425ee8e003877606e4f323b
jurgenstevens <jurgen.stevens@yahoo.com>
2024-09-11 19:39:50 -0500

    Include port 8080 in appsettings.json file.

--- obj/Debug/net8.0/dotnet-flights-mvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-flights-mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-flights-mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b94abd931fb0afb6e1e49adeb914896b43059b54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dfee8298b2493381510a831d76f84542aee76e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-flights-mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-flights-mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
