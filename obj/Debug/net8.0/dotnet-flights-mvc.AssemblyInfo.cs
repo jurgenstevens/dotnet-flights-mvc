93c118e270213de020a77b96d3a352f7cbeafd6d
jurgenstevens <jurgen.stevens@yahoo.com>
2024-09-12 22:47:18 -0500

    Include infra directory from the sample app for resources.bicep resources.

--- obj/Debug/net8.0/dotnet-flights-mvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-flights-mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-flights-mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21d761ce40d0d22a7425ee8e003877606e4f323b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd003af158d55acdb8e08c75e9dc82d73433e364")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-flights-mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-flights-mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
